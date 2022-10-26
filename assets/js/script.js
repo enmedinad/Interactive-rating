6d48695760f73da3d09ffadf27af06917aec2bfd
enmedinad <108096900+enmedinad@users.noreply.github.com>
2022-10-25 23:36:25 -0300

    Finished

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.submit-after-box').hide()
+});
+$('.option').click(function(){
+    //reset class
+    $('.submit-options').find('.option').attr('class', 'option');
+    //class add for visual interaction
+    $(this).addClass('checked');
+
+    //Value selected printed in html xd
+    $('#selected-value').html(
+        $(this).html()
+    );
+});
+
+
+$('.submit-button').click(function(){
+    //check if empty
+    if($('#selected-value').html() == ''){
+        alert('Debe seleccionar su puntuacion primero')
+    }else(
+        $('.submit-after-box').show()
+    )
+});
+
+
+
+
